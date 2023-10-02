5aa68b89afda7ea9b345fc4f13e04dca88e85a38
maxvdkelft0 <m.van.de.kelft@sintlucasedu.nl>
2023-10-02 15:41:21 +0200

    update

--- Assets/Scripts/ball.cs
+++ Assets/Scripts/ball.cs
@@ -6,19 +6,38 @@ public class ball : MonoBehaviour
 {
     public float Xposition = 0f;
     public float Yposition = 0f;
+    public float xSpeed;
+    public float ySpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(Xposition, Yposition, 0);
+        //transform.position = new Vector3(Xposition, Yposition, 0);
+        xSpeed = 1f;
+        ySpeed = 1f;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        Xposition = Xposition + 1f * Time.deltaTime;
-        Yposition = Yposition + 1f * Time.deltaTime;
+        Xposition += xSpeed * Time.deltaTime;
+        Yposition += ySpeed * Time.deltaTime;
         transform.position = new Vector3(Xposition, Yposition, 0);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("horizontalWall"))
+        {
+            Debug.Log("Touched horizontal wall");
+            ySpeed = ySpeed * -1;
+        }
+        if (collision.gameObject.CompareTag("verticalWall"))
+        {
+            Debug.Log("Touched vertical wall");
+            xSpeed = xSpeed * -1;
+        }
+    }
+
 }
